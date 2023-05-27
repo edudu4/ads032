2d0678a5986ce83f3c15782a81ee407b8f5472d5
unknown <2014290030@ACADEMICO.IESB.BR>
2023-05-26 22:09:43 -0300

    criando contexto de contatos

--- pratica02/src/pages/Home.jsx
+++ pratica02/src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import ContatosContext from "../contexts/ContatosContext"
+
+
 export default function Home() {
+    const {meusContatos} = useContext(ContatosContext)
     return (
         <h2>Meus Contatos</h2>
     )
--- pratica02/src/pages/Novo.jsx
+++ pratica02/src/pages/Novo.jsx
@@ -1,5 +1,24 @@
+import { useForm } from 'react-hook-form'
 export default function Novo() {
+    const { register, handleSubmit, formState: {errors} } = useForm()
+    const onSubmit = (data) => {
+
+    }
+
     return (
-        <h2>Novo Contato</h2>
+        
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id='nome' {...register('nome', {required: 'Campo obrigatório'})}/>
+                {errors.nome && <p>{errors.nome.message}</p>}
+            </div>
+            <div>
+                <label htmlFor="telefone">Telefone</label>
+                <input type="tel" id='telefone' {...register('telefone', {required: 'Campo obrigatório'})}/>
+                {errors.telefone && <p>{errors.telefone.message}</p>}
+            </div>
+            <button>Enviar</button>
+        </form>
     )
 }
